640d94f6f17a701e36475762b7a1ba4d64ebf0d9
waiyee <103561708+waiyeewwy@users.noreply.github.com>
2023-06-29 16:58:16 +1000

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import Flask, render_template
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
