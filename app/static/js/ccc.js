93ce53d7c5033c44b14b288ceb3faf4400e01207
waiyee <103561708+waiyeewwy@users.noreply.github.com>
2023-06-29 10:17:36 +1000

    Initial Commit

---
+++ app/static/js/ccc.js
@@ -0,0 +1,9 @@
+function faqdrop(){
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+}
